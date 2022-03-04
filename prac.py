0c57a33a3c6a411c967f5cae364c1f91a485ed8b
lightz51 <awkhan611@gmail.com>
2022-03-04 21:18:27 +0530

    Kivy Design file and how to load it

--- prac.py
+++ prac.py
@@ -1,43 +1,25 @@
 import kivy
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+# To specify the kv design file.
+# from kivy.lang import Builder
+# Builder.load_file('my.kv')
 
 
-class MyGridLayout(GridLayout):
+class MyGridLayout(Widget):
     def __init__(self, **kwargs):
         super(MyGridLayout, self).__init__(**kwargs)
 
-        self.cols = 1
-        self.row_force_default = True
-        self.row_default_height = 100
-        self.inner_grid = GridLayout(row_force_default=True, row_default_height=50)
-        self.inner_grid.cols = 2
-        self.inner_grid.add_widget(Label(text="Name: "))
-        # self.inner_grid.add_widget(Label(text="Name: ", size_hint_y=None, height=50))
+    name = ObjectProperty(None)
+    pwd = ObjectProperty(None)
 
-        self.name = TextInput(multiline=False)
-        self.inner_grid.add_widget(self.name)
-
-        self.inner_grid.add_widget(Label(text="Password: "))
-
-        self.password = TextInput(multiline=False)
-        self.inner_grid.add_widget(self.password)
-
-        self.add_widget(self.inner_grid)
-
-        self.submit = Button(text="Submit", size_hint_y=None, height=50)
-        self.submit.bind(on_press=self.press)
-        self.add_widget(self.submit)
-
-    def press(self, instance):                                                          # instance is like event, in this context.
+    def press(self):                                                          # instance is like event, in this context.
         name = self.name.text                                                           # fetching textinput of variable self.name
-        pwd = self.password.text
-        self.add_widget(Label(text=f"Name:{name}\nPassword:{pwd}"))
-
-        #Clear the input boxes
+        pwd = self.pwd.text
+        # self.add_widget(Label(text=f"Name:{name}\nPassword:{pwd}"))
+        print(f"Name:{name}\nPassword:{pwd}")
+        # Clear the input boxes
         self.name.text = ""
         self.password.text = ""
 
