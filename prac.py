e2fb48400f627402026cd2a1f7e1631eeb246eec
lightz51 <awkhan611@gmail.com>
2022-03-04 16:28:59 +0530

    Kivy Label

--- prac.py
+++ prac.py
@@ -1,1 +1,13 @@
-print("hello git")
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+
+
+class myApp(App):
+    def build(self):
+        return Label(text="Hello Kivy")
+
+
+if __name__ == '__main__':
+    myApp().run()
+
